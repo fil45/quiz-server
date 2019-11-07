c9b43af365d9da404b21f94571b96406c7ddaaa1
Sergey Filenko <filenko45@gmail.com>
2019-11-07 18:58:02 +0200

    Refactoring

--- constants.js
+++ constants.js
@@ -3,4 +3,8 @@ module.exports = {
   PORT: '1234',
   SUBJECTS_AMOUNT: 3,
   HASH: '$2b$10$PjqecawtIkC0tAPJhKjHGOH7N8KyZuhMNoQtP79fE.zGnoHnbjuxe',
+  DB_HOST: 'localhost',
+  DB_NAME: 'quiz',
+  DB_USER: 'root',
+  DB_PASSWORD: '12345678',
 };
--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const Sequelize = require('sequelize');
+const { DB_HOST, DB_NAME, DB_USER, DB_PASSWORD } = require('./constants');
 
-const db = new Sequelize('quiz', 'root', '12345678', {
-  host: 'localhost',
+const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
   dialect: 'mysql',
 });
 
